649dd584ebdb32a200cffe09dc2bb1fc2992d3ff
Philippe Coval <philippe.coval@astrolabe.coop>
2021-12-30 11:52:54 +0100

    emacs: Add local words .dir-locals.el
    Change-Id: I2809667532693c95f1203a94b7cccb2a0a702744
    Signed-off-by: Philippe Coval <philippe.coval@astrolabe.coop>

---
+++ .dir-locals.el
@@ -0,0 +1,18 @@
+;; https://emacs.stackexchange.com/questions/31396/can-i-instruct-ispell-to-insert-localwords-in-a-different-file#
+
+((org-mode
+  (ispell-buffer-session-localwords
+   "Accelerometer"
+   "AmbientLight"
+   "Fediverse"
+   "Geolocation"
+   "IoT"
+   "IoTivity"
+   "OSS"
+   "OpenSource"
+   "Rennes"
+   "Scalability"
+   "Tizen"
+   "WebThings"
+   "Yocto"
+   )))
